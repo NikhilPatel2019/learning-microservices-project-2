e9b6b18dd90bb8b53f3193ce8f34f4c5429fdf76
NikhilPatel2019 <nikhil.17.patel@gmail.com>
2025-01-18 00:41:54 +0530

    feat: error-handling

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -11,11 +13,10 @@ router.post('/api/users/signup', [
     const errors = validationResult(req);
 
     if(!errors.isEmpty()){
-        res.status(400).send(errors.array());
-        return;
+        throw new RequestValidationError(errors.array());
     }
-    const { email, password } = req.body;
-    console.log("creating a user...");
+    
+    throw new DatabaseConnectionError();
 
     res.send({})
 });
