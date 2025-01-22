630cc5a7d29e9b5431563e16f513123bbeef90f3
NikhilPatel2019 <nikhil.17.patel@gmail.com>
2025-01-22 15:11:22 +0530

    feat: signup-api

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -2,8 +2,8 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (reqq,res ) => {
-    res.send("Hello there!")
+router.get('/api/users/currentuser', (req,res ) => {
+    res.send("Hello there 123!")
 });
 
 export { router as currentUserRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,24 +1,39 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
+
+router.get('/api/users/currentuser2', (req,res ) => {
+    res.send("Hello there999!")
+});
 
 router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({min: 4, max: 20}).withMessage('Password must be between 4 to 20 characters'),
 ], 
-(req: Request ,res: Response ): void => {
+async (req: Request ,res: Response ): Promise<void> => {
     const errors = validationResult(req);
 
     if(!errors.isEmpty()){
         throw new RequestValidationError(errors.array());
     }
-    
-    throw new DatabaseConnectionError();
 
-    res.send({})
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if(existingUser){
+        throw new BadRequestError('Email in use');
+    }
+
+    const user = await User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
 });
 
 export { router as signupRouter };
